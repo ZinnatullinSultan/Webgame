584d0606ca91add5ab01b4f8fff3ec3c223400ef
ZinnatullinSultan <zinnatullin.969@gmail.com>
2023-12-01 02:56:17 +0300

    01.12 new

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
-
-// document.addEventListener("click", handleClick1);
-
-// function handleClick1(event) {
-//   const footer = document.getElementById('myfooter');
-//   const bars = document.getElementById('bars');
-//   bars.style.opacity = 0.5;
-//   footer.hidden = !footer.hidden;
-//   document.removeEventListener("click", handleClick1);
-// }
-
-
-// function handleClick2(event) {
-//   const bars = document.getElementById('bars');
-//   const cat = document.getElementById('cat');
-//   bars.style.opacity = 1;
-//   cat.style.opacity = 0.5;
-//   document.removeEventListener("click", handleClick2);
-// }
-
-// document.addEventListener("click", handleClick2);
-
 let step = 1;
-
 function handleClick(event) {
   const bars = document.getElementById('bars');
   const cat = document.getElementById('cat');
   const footer = document.getElementById('myfooter');
-
+  const who = document.getElementById('who');
+  const text = document.getElementById('text');
   if (step === 1) {
     bars.style.opacity = 0.5;
     footer.hidden = !footer.hidden;
@@ -34,64 +12,37 @@ function handleClick(event) {
   } else if (step === 2) {
     bars.style.opacity = 1;
     cat.style.opacity = 0.5;
+    who.innerText="Барс";
+    who.style.textAlign= 'right';
+    text.innerText="Здравствуйте! Я - Барс, символ республики Татарстан. Добро пожаловать в Казань! Это невероятный город, вы знаете, что здесь есть одна из самых красивых мечетей - Кул-Шариф?";
     step = 3;
   }
   else if (step === 3) {
+    text.innerText="Кул-Шариф - это историческая мечеть в Казани, построенная в 16 веке. Она была разрушена в 16 веке, но восстановлена в начале 21 века. Мечеть служит символом исламской культуры и истории Татарстана, и является одной из самых известных достопримечательностей города.";
+    step=4;
+  }
+  else if (step === 4) {
+    text.innerText="Татарская культура богата и разнообразна, она включает в себя множество традиций, обычаев, ремесел, музыки, танцев, кулинарии и многого другого. Татары - это тюркский народ, их культура имеет древние корни и передается из поколения в поколение. ";
+    step=5;
+  }
+  else if (step === 5) {
+    text.innerText="Одним из важных аспектов татарской культуры является музыка. Татарская музыка имеет свои уникальные инструменты, такие как кубыз , думбра  и многие другие.";
+    step=6;
+  }
+  else if (step === 6) {
+    text.innerText="Татарская кухня также заслуживает особого внимания. Она известна своими разнообразными блюдами, включая плов, манты, чак-чак.";
+    step=7;
+  }
+  else if (step === 7) {
+    bars.style.opacity = 0.5;
+    cat.style.opacity = 1;
+    who.innerText="Финик";
+    who.style.textAlign= 'left';
+    text.innerText="Спасибо, Барс!";
+    step = 8;
+  }
+  else if (step === 8) {
     document.addEventListener("click", handleClick);
   }
-
 }
-
 document.addEventListener("click", handleClick);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function handleClick2(event) {
-//   const bars = document.getElementById('bars1');
-//   bars.style.opacity = 0.5;
-//   footer.hidden = !footer.hidden;
-//   document.removeEventListener("click", handleClick2);
-// }
-// document.addEventListener("click", handleCli);
-
-// document.addEventListener('click', function(event) {
-//   // Получаем элементы footer и изображения
-//   const footerElement = document.querySelector('footer');
-//   const imageElement = document.querySelector('.bar');
-
-//   footerElement.hidden=!footerElement.hidden;
-//   imageElement.style.opacity = 0.5; 
-//   }
-// );
-// // Добавляем слушатель события клика на кнопку "Далее"
-// document.getElementById('next').addEventListener('click', function() {
-//   // Получаем элементы изображений
-//   const image1Element = document.getElementById('ca');
-//   const image2Element = document.getElementById('bar');
-
-//   // Проверяем, прозрачность первого изображения
-//   if (image1Element.style.opacity !== '0') {
-//     // Если первое изображение не полностью непрозрачно, делаем его непрозрачным
-//     image1Element.style.opacity = '0';
-//     image2Element.style.opacity = '1'; // Второе изображение полностью непрозрачно
-//   } else {
-//     // Если первое изображение полностью непрозрачно, делаем его полупрозрачным
-//     image1Element.style.opacity = '0.5'; // Пример полупрозрачности в 50%
-//     image2Element.style.opacity = '1'; // Второе изображение полностью непрозрачно
-//   }
-// });
-
